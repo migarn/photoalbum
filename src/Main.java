6e885d5a8c5377723ad25f6159cbf8b1db51d91a
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-17 15:45:58 +0200

    Project structure created

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import org.hibernate.Session;
+
+public class Main {
+
+	Session session;
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		// tu wstaw kod aplikacji
+		main.close();
+	}
+
+	public Main() {
+		session = HibernateUtil.getSessionFactory().openSession();
+	}
+
+	public void close() {
+		session.close();
+		HibernateUtil.shutdown();
+	}
+}
