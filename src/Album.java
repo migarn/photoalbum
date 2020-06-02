c3826d2eaeb537070eff08a425527a8779d099cd
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-02 16:55:10 +0200

    Album class initially implemented

--- src/Album.java
+++ src/Album.java
@@ -1,11 +1,13 @@
 import java.util.LinkedHashSet;
 import java.util.Set;
-
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +24,56 @@ public class Album {
 	@Column
 	private String description;
 	
+	@OneToMany(cascade=CascadeType.ALL)
+	//@JoinColumn(name="school_id")
 	private Set<Photo> photos = new LinkedHashSet<Photo>();
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(Set<Photo> photos) {
+		this.photos = photos;
+	}
+	
+	public void addPhoto(Photo photo) {
+		this.photos.add(photo);
+	}
+	
+	public void removePhoto(Photo photo) {
+		this.photos.remove(photo);
+	}
+	
+	public int getPhotosNumber() {
+		return this.photos.size();
+	}
+	
+	@Override
+	public String toString() {
+		return "Album \'" + getName() + "\': " + getDescription() + ". Number of photos: " + getPhotosNumber() + ".";
+	}
 }
